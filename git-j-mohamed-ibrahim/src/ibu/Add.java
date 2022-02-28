3f296a26fd55bc10b52494120e071653a9980ec9
MOHAMED IBRAHIM <MOHAMED IBRAHIM@DESKTOP-C9BVRP9>
2022-02-28 22:03:29 +0530

    My first commitment

---
+++ git-j-mohamed-ibrahim/src/ibu/Add.java
@@ -0,0 +1,24 @@
+package ibu;
+
+public class Add {
+	
+	public static void main(String[] args) {
+		System.out.println("");
+		
+		int first=10;
+		int second=20;
+		
+		System.out.println("Add two numbers: " + first + " + " + second);
+		System.out.println(" ");
+		
+		
+		
+		System.out.println("");
+		
+		int sum= first + second;
+		
+		System.out.println("the sum is = " + sum);
+		
+
+	}
+}
---
+++ git-j-mohamed-ibrahim/src/ibu/Addition.java
@@ -0,0 +1,22 @@
+package ibu;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		System.out.println("ENTER Three NUMBERS:");
+		
+		int first = 100;
+		int second= 200;
+		int third= 300;
+		
+		System.out.println(first + " " +  second + " " + third);
+		
+		int sum= first + second + third;
+		
+		System.out.println("the sum is:" + sum);
+		
+
+	}
+
+}
